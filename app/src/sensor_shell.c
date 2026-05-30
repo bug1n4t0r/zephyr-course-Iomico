ec75a0d10ca86f7f522866128e11770f29f98af5
Bugi <crazy.flips90@gmail.com>
2026-05-30 21:30:29 +0200

    add new sensor sub command

--- app/src/sensor_shell.c
+++ app/src/sensor_shell.c
@@ -1,4 +1,5 @@
 #include <bugis_driver.h>
+#include <stdlib.h>
 #include <zephyr/device.h>
 #include <zephyr/shell/shell.h>
 
@@ -34,12 +35,35 @@ static int cmd_info(const struct shell *sh, size_t argc, char **argv) {
   return 0;
 }
 
+static int cmd_set(const struct shell *sh, size_t argc, char **argv) {
+  char *end;
+  long  v = strtol(argv[1], &end, 10);
+
+  if (*end != '\0' || v < 0 || v > UINT8_MAX) {
+    shell_error(sh, "value must be 0-255");
+    return -EINVAL;
+  }
+
+  uint8_t u8  = (uint8_t)v;
+  int     ret = bugis_set_val(sensor_dev, &u8);
+
+  uint8_t get;
+  bugis_get_val(sensor_dev, &get);
+
+  if (ret < 0) {
+    shell_error(sh, "set failed: %d", ret);
+  } else {
+    shell_print(sh, "set ok: %d", get);
+  }
+  return ret;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(
-    sensor_cmds,
-    SHELL_CMD(fetch, NULL, "Call sensor_sample_fetch", cmd_fetch),
-    SHELL_CMD(read, NULL, "Call sensor_channel_get and print result",
-              cmd_read),
+    sensor_cmds, SHELL_CMD(fetch, NULL, "Call sensor_sample_fetch", cmd_fetch),
+    SHELL_CMD(read, NULL, "Call sensor_channel_get and print result", cmd_read),
     SHELL_CMD(info, NULL, "Print device name and ready state", cmd_info),
+    SHELL_CMD_ARG(set, NULL, "Call bugis_set_val <value> (0-255)", cmd_set, 2,
+                  0),
     SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(sensor, &sensor_cmds, "Sensor shell commands", NULL);
